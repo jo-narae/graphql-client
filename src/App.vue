14e62047957cd5d2ac99ae4b7a0e99fbdf4a672d
jo-narae <nare6004@gmail.com>
2021-05-01 12:51:25 +0900

    메인 화면 처리

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
   <v-app id="inspire">
-    <!-- <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-    </v-navigation-drawer> -->
-
     <v-app-bar app>
-      <v-app-bar-nav-icon>
-        <v-icon>mdi-chevron-left</v-icon>
+      <v-app-bar-nav-icon @click="moveToPage">
+        <v-icon>{{ appbarIcon }}</v-icon>
       </v-app-bar-nav-icon>
 
-      <v-toolbar-title>Artists Of Agency</v-toolbar-title>
+      <v-toolbar-title class="hand-pointer" @click="moveToHome">Artists Of Agency</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -22,9 +16,33 @@
 
 <script>
   export default {
-    data: () => ({ drawer: false }),
+    data() {
+      return {
+        appbarIcon: 'mdi-music'
+      }
+    },
+    watch: {
+      async $route(to) {
+        if(to.path == '/') this.appbarIcon = 'mdi-music';
+        else this.appbarIcon = 'mdi-chevron-left';
+      }
+    },
+    methods: {
+      moveToPage() {
+        this.$router.go(-1);
+      },
+      moveToHome() {
+        this.$router.push({ path: '/' });
+      }
+    },
     created() {
       this.$vuetify.theme.dark = true;
     }
   }
 </script>
+
+<style>
+.hand-pointer {
+  cursor: pointer;
+}
+</style>
